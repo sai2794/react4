6356dbf2130fa480bf34cf1c2706d20e8ff20310
Sai Deepthi <saideepthi2794@mail.com>
2023-10-30 19:54:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+"use client"
+import { useEffect, useState } from 'react';
 import './App.css';
-
+import Table from './Table';
+import axios from 'axios';
 function App() {
+  const [userdata,setUserdata] = useState([]);
+  const getData = async ()=>{
+    try{
+      const res = await axios.get("https://dummyjson.com/users");
+      setUserdata(res.data.users);
+    }
+    catch(error){
+      console.error("Error fetching the data",error);
+    }
+  }
+  useEffect(()=>{
+    getData();
+  },[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Dummy Data</h1>
+      <br/>
+      <Table data = {userdata}/>
     </div>
   );
 }
-
 export default App;
